45cb7346fe5b488a3b8618d763fbd03efb3edb71
Younes Rabeh <93033194+YounesRabeh@users.noreply.github.com>
2024-04-16 17:01:38 +0200

    Update infinity.asm

--- infinity.asm
+++ infinity.asm
@@ -2,11 +2,41 @@
 
 .global main
 main:
-    ldi r16, 0xFF   ; Set port D as output
+    ; Initialize stack pointer
+    ldi r16, low(RAMEND)
+    out SPL, r16
+    ldi r16, high(RAMEND)
+    out SPH, r16
+    
+    ; Initialize port D as output
+    ldi r16, 0xFF   ; Set port B as output
     out DDRB, r16
 
 loop:
+    ; Turn on LED
     ldi r16, (1 << LED_PIN)  ; Set bit corresponding to LED_PIN
     out PORTB, r16    ; Turn on LED
+    
+    ; Delay
+    ldi r20, 0xFF
+    ldi r21, 0xFF
+delay_loop:
+    dec r21
+    brne delay_loop
+    dec r20
+    brne delay_loop
+    
+    ; Turn off LED
+    ldi r16, (0 << LED_PIN)  ; Clear bit corresponding to LED_PIN
+    out PORTB, r16    ; Turn off LED
+    
+    ; Delay
+    ldi r20, 0xFF
+    ldi r21, 0xFF
+delay_loop_off:
+    dec r21
+    brne delay_loop_off
+    dec r20
+    brne delay_loop_off
+    
     rjmp loop         ; Loop infinitely
-
