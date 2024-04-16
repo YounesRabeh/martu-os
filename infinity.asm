bfccca4bc7e20f1a5e95f65885bd8bd6a20cc26a
Younes Rabeh <93033194+YounesRabeh@users.noreply.github.com>
2024-04-16 17:00:57 +0200

    Update infinity.asm

--- infinity.asm
+++ infinity.asm
@@ -1,1 +1,12 @@
+.equ LED_PIN, 13   ; Define LED pin number
+
+.global main
+main:
+    ldi r16, 0xFF   ; Set port D as output
+    out DDRB, r16
+
+loop:
+    ldi r16, (1 << LED_PIN)  ; Set bit corresponding to LED_PIN
+    out PORTB, r16    ; Turn on LED
+    rjmp loop         ; Loop infinitely
 
